1c88a6c22c645f84d6fd81ea89c84ece659642d0
KrisChe8 <krischernobai@gmail.com>
2024-02-05 22:37:56 +0200

    complete view an individual article

---
+++ src/components/SingleArticleManager.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import SingleArticleCard from './SingleArticleCard';
+
+export default function SingleArticleManager({activeUserName}){
+    const [voteArticle, setVoteArticle] = useState(null)
+    const [commentId, setCommentId] = useState(null)
+    const [commentsVote, setCommentsVote] = useState(null)
+    
+    const [article, setArticle] = useState({})
+
+    const {id}  = useParams();
+    useEffect(()=>{
+        axios.get(`https://nc-news-24h6.onrender.com/api/articles/${id}`).then((response)=>{
+            setArticle(response.data.article)
+            })
+    }, [])
+    
+   
+
+    return(
+        <section>
+           <SingleArticleCard article={article} activeUserName={activeUserName} />
+        </section>
+    )
+}
